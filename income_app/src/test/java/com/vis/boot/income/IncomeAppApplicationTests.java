8600e8dca14ecae71ab245981888feb4c42b94ad
vishn <vishn@host.docker.internal>
2023-09-22 20:25:44 -0400

    Income initial checkin

---
+++ income_app/src/test/java/com/vis/boot/income/IncomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vis.boot.income;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
